197e48bae1e589bc34126399f383a30a17f85655
Sean <twmllsh@gmail.com>
2024-11-03 20:15:55 +0900

    금요일 ohlcv전체 데이터 다운로드 수정1

--- dashboard/utils/dbupdater.py
+++ dashboard/utils/dbupdater.py
@@ -430,7 +430,7 @@ class DBUpdater:
             ## 데이터 모두 먼저 지우기
             Ohlcv.objects.all().delete()
             with connection.cursor() as cursor:
-                cursor.execute("ALTER SEQUENCE mymodel_id_seq RESTART WITH 1;")
+                cursor.execute("ALTER SEQUENCE Ohlcv_id_seq RESTART WITH 1;")
             
             
             to_create_add = []
